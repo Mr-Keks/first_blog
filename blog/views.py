1e15e6c848e74435edd77c85d92b5458d36d3473
Mr Keks <levelel744@gmail.com>
2020-12-06 02:01:45 +0200

    Modified templates to display posts from database v1

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 def post_list(request):
-	me = User.objects.get(username = 'superadmin')
+	me = User.objects.get(username = 'superuser')
 	posts = Post.objects.filter(author = me)
 	return render(request, 'blog/post_list.html', {'posts':posts})
